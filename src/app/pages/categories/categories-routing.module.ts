914a4b37f5610fec97f3f1540ae56e9655ddc2db
Itatiano de Tarso Maciel Silva <itatiano.tarso@tjma.jus.br>
2018-12-06 10:56:01 -0300

    adding in-memory-database for categories

--- src/app/pages/categories/categories-routing.module.ts
+++ src/app/pages/categories/categories-routing.module.ts
@@ -6,7 +6,7 @@ import { CategoryFormComponent } from './category-form/category-form.component';
 
 const routes: Routes = [
   { path : '', component: CategoryListComponent },
-  { path : ':id/new', component: CategoryFormComponent },
+  { path : ':new', component: CategoryFormComponent },
   { path : ':id/edit', component: CategoryFormComponent }
 ];
 
