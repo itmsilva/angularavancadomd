f231efe704cb3cd34a749d604ba224ae8b5b4bcc
Itatiano de Tarso Maciel Silva <itatiano.tarso@tjma.jus.br>
2018-12-07 07:49:39 -0300

    adding category model

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,7 +1,7 @@
 import { InMemoryDbService} from "angular-in-memory-web-api";
 
-export class InMemoryDatabase implements InMemoryDbService {
-    createDd(){
+export class InMemoryDatabase implements InMemoryDbService{
+    createDb(){
         const categories = [
             { id: 1, name: 'Moradia', description: 'Pagamentos de Contas da Casa'},
             { id: 2, name: 'Saúde', description: 'Plano de Saúde e Remédios'},
